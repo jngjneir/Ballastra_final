25e061e323296fee49587ca8c52c524a4b93bd19
lalitarana123456 <lalitakumari39840@gmail.com>
2025-11-22 18:19:33 +0530

    added

--- Backend/app.js
+++ Backend/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || ;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
